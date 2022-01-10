34fa13fe6610d60a386280f6052c74e7de2d2815
ViliAu <36374215+ViliAu@users.noreply.github.com>
2022-01-10 22:34:41 +0200

    MEAN stack start, express setup

---
+++ modules/5-MEAN/routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/register', (req, res) => {
+
+});
+
+router.get('/authenticate', (req, res) => {
+
+});
+
+router.get('/profile', (req, res) => {
+
+});
+
+router.get('/validate', (req, res) => {
+
+});
+
+module.exports = router
